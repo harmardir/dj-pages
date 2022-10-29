289a4ce87f364b241be10c8d9d53b1aa0ffee9ae
Harout <harmardir@gmail.com>
2022-10-29 18:34:42 +0300

    adding new profile page

--- pages/pages/tests.py
+++ pages/pages/tests.py
@@ -8,4 +8,8 @@ class SimpleTests(SimpleTestCase):
     def test_about_page_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
+    
+    def test_profile_page_status_code(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
 
--- pages/pages/views.py
+++ pages/pages/views.py
@@ -6,3 +6,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class ProfilePageView(TemplateView):
+    template_name = 'profile.html'
